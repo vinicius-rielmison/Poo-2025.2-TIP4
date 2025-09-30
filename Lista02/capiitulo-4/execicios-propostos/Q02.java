062f12630d06fccc39b42a6f52630bafec60b3ce
vinicius <rielmisonvinicius87@gmail.com>
2025-09-30 17:09:03 -0300

    Add files via upload

---
+++ Lista02/capiitulo-4/execicios-propostos/Q02.java
@@ -0,0 +1,30 @@
+package br.edu.principal;
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1 , nota2 ,media ;
+		
+		System.out.println("=========== CALCULO MEDIA ============= ");
+		System.out.println("digite sua primeira nota : ");
+		nota1  = sc.nextDouble();
+		System.out.println("digite sua segunda nota : ");
+		nota2  = sc.nextDouble();
+
+		
+		media = (nota1 + nota2  ) / 2 ;
+		if (media >= 7 ) {
+			System.out.println("voce foi aprovado direto parabens! : ");
+			System.out.println("sua media foi  : " + media );
+		}else if (media >=3 & media <=7 ) {
+			System.out.println("voce ficou de recuperação que pena !, mais não desista tem como recuperar : ");
+			System.out.println("sua media foi  : " + media );
+		}else {
+			System.out.println("voce reprovou direto que pena tente novamente no prximo semestre   : ");
+			System.out.println("sua media foi  : " + media );
+		}
+	}
+}
