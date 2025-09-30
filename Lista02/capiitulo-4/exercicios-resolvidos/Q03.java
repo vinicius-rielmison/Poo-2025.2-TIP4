0513d8e3e74a23b78a0a86e3e7bbc689f2289b08
vinicius <rielmisonvinicius87@gmail.com>
2025-09-30 18:18:49 -0300

    Add files via upload

---
+++ Lista02/capiitulo-4/exercicios-resolvidos/Q03.java
@@ -0,0 +1,10 @@
+package br.edu.principal;
+
+public class Principal {
+	    public static void main(String[] args) {
+	        for (int i = 0; i < 100; i++) {
+	            System.out.println((i + 1) + " - Hello World");
+	        }
+	    }
+	}
+
